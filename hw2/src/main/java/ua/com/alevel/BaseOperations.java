7bfc378f560cc709364f84b4b5bf833f0732e239
ilyapoguliaev <bigtapok@icloud.com>
2023-08-26 19:27:30 +0300

    commit

---
+++ hw2/src/main/java/ua/com/alevel/BaseOperations.java
@@ -0,0 +1,10 @@
+package ua.com.alevel;
+
+public class BaseOperations {
+
+    public static void main(String[] args) {
+//        new PrimitiveType().test();
+//        new Operations().circle();
+        new Operations().ifSwitch();
+    }
+}
